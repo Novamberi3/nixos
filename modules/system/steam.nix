cfb14a159bf60c4326ee1436da10c1374ef0e003
novamberi3 <novamber@tutamail.com>
2026-08-31 00:52:38 +0100

    added steam

---
+++ modules/system/steam.nix
@@ -0,0 +1,35 @@
+{ pkgs, inputs, ... }: {
+  imports = [ inputs.nix-gaming.nixosModules.platformOptimizations ];
+
+  programs = {
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = false;
+
+      gamescopeSession.enable = true;
+
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
+
+      platformOptimizations.enable = true;
+      package = pkgs.steam.override {
+        extraPkgs =
+          pkgs: with pkgs; [
+            libkrb5
+            keyutils
+          ];
+      };
+    };
+
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+      args = [
+        "--rt"
+        "--expose-wayland"
+      ];
+    };
+  };
+}
